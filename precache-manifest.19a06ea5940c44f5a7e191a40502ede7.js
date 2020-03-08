c578af7d1a95156ad23d958136aea426723c20e2
Prabhat Doongarwal <prabhat.doongarwal@leoforce.com>
2020-03-08 18:46:34 +0530

    Add image classifer

---
+++ precache-manifest.19a06ea5940c44f5a7e191a40502ede7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ee2f45020640a5a3a8c75d42db70eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "285e45a6ac651c14e037",
+    "url": "/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "0b3b01f2b75830679859",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "285e45a6ac651c14e037",
+    "url": "/static/js/2.871b1152.chunk.js"
+  },
+  {
+    "revision": "9856faae55210d330bc1af8176834bf4",
+    "url": "/static/js/2.871b1152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3b01f2b75830679859",
+    "url": "/static/js/main.d3a2f406.chunk.js"
+  },
+  {
+    "revision": "c59cdd54b46956ed1896",
+    "url": "/static/js/runtime-main.1932ac3c.js"
+  }
+]);
